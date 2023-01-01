ab70fc60cf4be66cecabc7e562bdc8e6f4d547e5
rich boundji <boundjirichrich@gmail.com>
2022-12-31 21:15:07 -0700

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Rich Boundji on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
